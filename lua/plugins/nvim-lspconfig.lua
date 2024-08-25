8ad8b3ac67e8c2b6cf79cd18baf1a4b67ccc94e1
makinzm <nozomi.maki.da@gmail.com>
2024-08-25 12:15:47 +0900

    feat: typescript

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,7 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
+      -- python
       pyright = {
         filetypes = { "python" },
         root_dir = require("lspconfig.util").root_pattern(".venv"),
@@ -12,6 +13,7 @@ return {
           client.server_capabilities.documentRangeFormattingProvider = false
         end,
       },
+      -- rust
       taplo = {
         keys = {
           {
@@ -24,6 +26,48 @@ return {
               end
             end,
             desc = "Show Crate Documentation",
+          },
+        },
+      },
+      -- typescript
+      tsserver = {
+        enabled = false,
+      },
+      vtsls = {
+        -- explicitly add default filetypes, so that we can extend
+        -- them in related extras
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+        },
+        settings = {
+          complete_function_calls = true,
+          vtsls = {
+            enableMoveToFileCodeAction = true,
+            autoUseWorkspaceTsdk = true,
+            experimental = {
+              completion = {
+                enableServerSideFuzzyMatch = true,
+              },
+            },
+          },
+          typescript = {
+            updateImportsOnFileMove = { enabled = "always" },
+            suggest = {
+              completeFunctionCalls = true,
+            },
+            inlayHints = {
+              enumMemberValues = { enabled = true },
+              functionLikeReturnTypes = { enabled = true },
+              parameterNames = { enabled = "literals" },
+              parameterTypes = { enabled = true },
+              propertyDeclarationTypes = { enabled = true },
+              variableTypes = { enabled = false },
+            },
           },
         },
       },
